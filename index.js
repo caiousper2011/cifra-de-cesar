10123b69b30cf865761d663c8751ff213acbec6e
Caio Barbosa de Almeida <32861462+caiousper2011@users.noreply.github.com>
2021-09-02 08:50:11 -0300

    refactor(alphabet): reestruturação do alfabeto

--- index.js
+++ index.js
@@ -1,29 +1,42 @@
 'use strict';
 const BEGIN_ENCODE_NUMBER_AT = 1;
-const alphabetCharacters = [...Array(26)]
-  .map((_, i) => i + 65)
-  .map((number) => String.fromCharCode(number));
-
 const encodeTextElement = document.querySelector('#txt-encode');
 const decodeTxtElement = document.querySelector('#txt-decode');
 const btnEncode = document.querySelector('#btn-encode');
 const btnDecode = document.querySelector('#btn-decode');
 const encodeNumberElement = document.querySelector('#encode-selection');
 const importFileElement = document.querySelector('#input-import-file');
+const alphabetSelectElement = document.querySelector('#alphabet-txt');
+let alphabetCharacters = [];
 let encodeNumberAt = 1;
+const alphabets = [
+  {
+    label: 'Alfabeto latino - ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+    value: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+    replacers: { A: '[ÀÁÂÃÄÅàáâãäå]', E: '[ÈÉÊË]', C: '[Ç]' },
+  },
+  {
+    label: 'Alfabeto grego - ΑΒΓΔΕΖΗΘΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩ',
+    value: 'ΑΒΓΔΕΖΗΘΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩ',
+  },
+  {
+    label: 'Alfabeto russo - АБВГДЕЁЖЗИЙІКЛМНОПРСТУФХЦЧШЩЪЫЬѢЭЮЯѲѴ',
+    value: 'АБВГДЕЁЖЗИЙІКЛМНОПРСТУФХЦЧШЩЪЫЬѢЭЮЯѲѴ',
+  },
+];
 
-const expressionsToReplaceAccentedWords = () => {
-  const charactersToBeReplaced = { A: '[ÀÁÂÃÄÅàáâãäå]', E: '[ÈÉÊË]', C: '[Ç]' };
-  return Object.entries(charactersToBeReplaced).reduce(
-    (regs, [key, expression]) => {
-      return { ...regs, [key]: new RegExp(expression, 'gi') };
-    },
-    {},
-  );
+const expressionsToReplaceAccentedWords = (replacers) => {
+  return Object.entries(replacers).reduce((regs, [key, expression]) => {
+    return { ...regs, [key]: new RegExp(expression, 'gi') };
+  }, {});
 };
 
-const replaceAccentedWords = (text) => {
-  const replacementExpressions = expressionsToReplaceAccentedWords();
+const replaceAccentedWords = (text, replacers) => {
+  if (!replacers) {
+    return text;
+  }
+
+  const replacementExpressions = expressionsToReplaceAccentedWords(replacers);
   return Object.entries(replacementExpressions).reduce(
     (text, [commonWord, expression]) => {
       return text.replace(expression, commonWord);
@@ -32,35 +45,43 @@ const replaceAccentedWords = (text) => {
   );
 };
 
-const getNextWordBasedOnEncodeNumber = (wordIndex, encodeNumber) => {
+const getNextWordBasedOnEncodeNumber = (alphabet, wordIndex, encodeNumber) => {
   const nextWord = wordIndex + encodeNumber;
 
-  if (nextWord >= alphabetCharacters.length) {
-    return alphabetCharacters[nextWord - alphabetCharacters.length];
+  if (nextWord >= alphabet.length) {
+    return alphabet[nextWord - alphabet.length];
   }
 
-  return alphabetCharacters[nextWord];
+  return alphabet[nextWord];
 };
 
-const getPreviousWordBasedOnEncodeNumber = (wordIndex, encodeNumber) => {
+const getPreviousWordBasedOnEncodeNumber = (
+  alphabet,
+  wordIndex,
+  encodeNumber,
+) => {
   const previousWord = wordIndex - encodeNumber;
 
   if (previousWord < 0) {
-    return alphabetCharacters[previousWord + alphabetCharacters.length];
+    return alphabet[previousWord + alphabet.length];
   }
 
-  return alphabetCharacters[previousWord];
+  return alphabet[previousWord];
 };
 
-const getWordIndex = (word) =>
-  alphabetCharacters.findIndex((alphabetWord) => word === alphabetWord);
+const getWordIndex = (alphabet, word) =>
+  alphabet.findIndex((alphabetWord) => word === alphabetWord);
 
-const encode = (text, encodeNumber) => {
+const encode = (alphabet) => (text, encodeNumber) => {
   return text.replace(/./gi, (word) => {
-    const currentWordIndex = getWordIndex(word);
+    const currentWordIndex = getWordIndex(alphabet, word);
 
     if (currentWordIndex !== -1) {
-      return getNextWordBasedOnEncodeNumber(currentWordIndex, encodeNumber);
+      return getNextWordBasedOnEncodeNumber(
+        alphabet,
+        currentWordIndex,
+        encodeNumber,
+      );
     }
 
     return word;
@@ -69,18 +90,23 @@ const encode = (text, encodeNumber) => {
 
 const encodeCharacteres = () => {
   const text = encodeTextElement.value;
-  const formattedText = prepareText(text);
-  const convertedText = encode(formattedText, encodeNumberAt);
+  const formattedText = prepareText(text, alphabetCharacters);
+  const encodePrepared = encode(alphabetCharacters);
+  const convertedText = encodePrepared(formattedText, encodeNumberAt);
 
   decodeTxtElement.value = convertedText;
 };
 
-const decode = (text, encodeNumber) => {
+const decode = (alphabet) => (text, encodeNumber) => {
   return text.replace(/./gi, (word) => {
-    const currentWordIndex = getWordIndex(word);
+    const currentWordIndex = getWordIndex(alphabet, word);
 
     if (currentWordIndex !== -1) {
-      return getPreviousWordBasedOnEncodeNumber(currentWordIndex, encodeNumber);
+      return getPreviousWordBasedOnEncodeNumber(
+        alphabet,
+        currentWordIndex,
+        encodeNumber,
+      );
     }
 
     return word;
@@ -89,15 +115,20 @@ const decode = (text, encodeNumber) => {
 
 const decodeCharacteres = () => {
   const text = decodeTxtElement.value;
-  const formattedText = prepareText(text);
-  const convertedText = decode(formattedText, encodeNumberAt);
+  const formattedText = prepareText(text, alphabetCharacters);
+  const decodePrepared = decode(alphabetCharacters);
+  const convertedText = decodePrepared(formattedText, encodeNumberAt);
 
   encodeTextElement.value = convertedText;
 };
 
-const prepareText = (text) => {
+const prepareText = (text, selectedAlphabet) => {
+  const { replacers } = alphabets.find(
+    ({ value }) => value == selectedAlphabet.join(''),
+  );
   const textFormattedToUpperCase = text.toUpperCase();
-  return replaceAccentedWords(textFormattedToUpperCase);
+
+  return replaceAccentedWords(textFormattedToUpperCase, replacers);
 };
 
 const importFileText = async ({
@@ -109,16 +140,37 @@ const importFileText = async ({
   encodeTextElement.value = text;
 };
 
+const generateAlphabetListIntoAlphabetSelect = () => {
+  alphabets.forEach(({ value, label }, index) => {
+    const option = document.createElement('option');
+
+    if (!Boolean(index)) {
+      option.selected = true;
+      alphabetSelected({ target: { value } });
+    }
+
+    option.value = value;
+    option.textContent = label;
+    alphabetSelectElement.appendChild(option);
+  });
+};
+
+const alphabetSelected = ({ target: { value } }) => {
+  alphabetCharacters = value.split('');
+  encodeNumberElement.setAttribute('max', alphabetCharacters.length);
+};
+
 const init = () => {
   importFileElement.addEventListener('change', importFileText);
-  encodeNumberElement.setAttribute('max', alphabetCharacters.length);
   encodeNumberElement.setAttribute('value', BEGIN_ENCODE_NUMBER_AT);
   btnEncode.addEventListener('click', encodeCharacteres);
   btnDecode.addEventListener('click', decodeCharacteres);
+  alphabetSelectElement.addEventListener('change', alphabetSelected);
   encodeNumberElement.addEventListener(
     'change',
     ({ target: { value } }) => (encodeNumberAt = Number(value)),
   );
+  generateAlphabetListIntoAlphabetSelect();
 };
 
 init();

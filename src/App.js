4c434509acfc88c121381a1525c0df07e718d220
Caio Barbosa de Almeida <32861462+caiousper2011@users.noreply.github.com>
2021-09-19 17:51:51 -0300

    refactor(reat): criação do componente dos campos de texto

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
 import Controls from "./components/Controls/Controls.js";
 import FooterControls from "./components/FooterControls/FooterControls.js";
+import Textarea from "./components/Textarea/Textarea.js";
 import ProvidersContainer from "./context/index.js";
 
 export const App = () => {
+  const [encodeText, setEncodeText] = React.useState("");
+  const [decodeText, setDecodeText] = React.useState("");
+
   return (
     <ProvidersContainer>
-      <Controls />
-      <FooterControls />
+      <header>
+        <h3>Encriptador de Cesar</h3>
+      </header>
+      <main>
+        <div className="backdrop">
+          <div className="container">
+            <section className="main-section">
+              <header className="header-page">
+                <h1 className="title">Encriptador Cifra de Cesar</h1>
+                <h6 className="description">
+                  Criptografe facilmente seus textos com apenas um clique!
+                </h6>
+              </header>
+
+              <main className="content">
+                <Textarea text={encodeText} handleText={setEncodeText} />
+                <Controls />
+                <Textarea text={decodeText} handleText={setDecodeText} />
+                <FooterControls />
+              </main>
+            </section>
+          </div>
+        </div>
+        <section className="alphabet-container">
+          <select className="field field-outline" type="text" id="alphabet-txt">
+            <option value="">Selecione um alfabeto</option>
+          </select>
+          <label
+            className="field button button-outline"
+            htmlFor="input-import-file"
+          >
+            importar texto
+          </label>
+          <input type="file" id="input-import-file" accept=".txt" />
+        </section>
+      </main>
     </ProvidersContainer>
   );
 };
